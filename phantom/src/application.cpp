026877c3fd794127d465a48f9199ddc7372d85e8
karanvivekbhargava <karanb@umd.edu>
2019-03-24 22:34:21 -0400

    GL - Fixed linking with openGL in cmake
    Added a cute pink screen of death using opengl.

--- phantom/src/application.cpp
+++ phantom/src/application.cpp
@@ -18,8 +18,11 @@ namespace Phantom
 //        PHTM_CORE_TRACE("Welcome to Phantom Engine!");
 //        WindowResizeEvent e(1280, 720);
 //        PHTM_CORE_TRACE(e);
+	
         while(m_is_running)
         {
+            glClear(GL_COLOR_BUFFER_BIT);
+	    glClearColor(1, 0, 1, 1);
             m_window->OnUpdate();
         }
     }
